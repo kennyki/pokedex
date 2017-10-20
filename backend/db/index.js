80f6bcf64e36c443d64a0753a377a2e3a1278eef
kenny ki <knyki.12@gmail.com>
2017-10-20 15:51:10 +0800

    init expressjs app

---
+++ backend/db/index.js
@@ -0,0 +1,12 @@
+var mongoose = require('mongoose');
+
+mongoose.connect('mongodb://localhost/pokedex');
+
+var db = mongoose.connection;
+
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function() {
+  console.log('Connected to pokedex!')
+});
+
+export default db;
